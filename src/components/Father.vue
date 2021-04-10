9295cfcc94780ed6339ea3eaaab5f11a6b6a2376
dandan1232 <345652127@qq.com>
2021-04-10 13:33:42 +0800

    vue3快速起步文档学习210410

---
+++ src/components/Father.vue
@@ -0,0 +1,15 @@
+<!--Father.vue-->
+<template>
+  <div>我是父亲</div>
+  <Son />
+</template>
+
+<script>
+  import Son from "./Son.vue";
+  export default {
+    name: "Father",
+    components: {
+      Son,
+    },
+  };
+</script>
---
+++ src/remove.js
@@ -0,0 +1,12 @@
+// remove.js
+// state 从 App.vue 传入
+export default function useRemove(state) {
+  // 添加学生方法
+  const removeStudent = (_index) => {
+    state.studentList = state.studentList.filter(
+      (item, index) => _index !== index
+    );
+  };
+
+  return { removeStudent };
+}
